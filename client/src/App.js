5d20d9133fd95fa25cc377cde6e5849020595b33
unknown <candygarcia014@gmail.com>
2021-01-18 18:56:37 -0800

    commiting all

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Nav from "./components/Nav"; <-- to call Navbar 
+
+function App() {
+  return (
+    <div>
+      <h1>Hello This is page 1</h1>
+    </div>
+  );
+}
+
+export default App;
